200cd017754a9a49a7244e90cbedaa8029515101
shapirab <shapirab@jamd.ac.il>
2025-08-21 19:43:58 +0300

    21/8/25 hooked home component with login and register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/accounts/login/login.component';
+import { RegisterComponent } from './features/accounts/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
