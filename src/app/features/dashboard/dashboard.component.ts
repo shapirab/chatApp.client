05554cf68c66765a2100a3568835f922146e1498
shapirab <shapirab@jamd.ac.il>
2025-12-07 14:36:55 +0200

    7/12/25 preliminary styling of dashboard

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { AccountService } from '../../core/services/account/account.service';
 import { Router } from '@angular/router';
+import { ChatItemToReturnDto } from '../../core/models/chatItem/chatItemToReturnDto';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   accountService = inject(AccountService);
+  selectedChatRoom?: ChatItemToReturnDto;
+  chatRooms:ChatItemToReturnDto[] = [];
+
   private router = inject(Router);
 
   logout(){
